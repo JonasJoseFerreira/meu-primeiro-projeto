63092fc06c03437a9d6f6847653cc15683157c36
Jonas <ferreirabentojonas@gmail.com>
2022-08-19 16:05:55 -0300

    Versão inicial do projeto 1

--- src/main/java/com/kazale/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/kazale/MeuPrimeiroProjetoApplication.java
@@ -3,6 +3,7 @@ package com.kazale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MeuPrimeiroProjetoApplication {
 
---
+++ src/test/java/com/kazale/MeuPrimeiroProjetoApplicationTest.java
@@ -0,0 +1,14 @@
+package com.kazale;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MeuPrimeiroProjetoApplicationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
